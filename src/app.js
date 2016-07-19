28949d3b6e8ea2b0f1b7fb6510123fb09037b050
RussellWatson <russellwatson@126.com>
2016-07-19 14:04:19 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -73,7 +73,7 @@ function errShow(item, err) {
 function asyncWrite(data, target, err, finish) {
     if (!repo)
         Spine.Route.navigate("");
-    repo.write("master", target, data, "simple",
+    repo.write("master", target, data, "shenple",
                function(e) {
                    var ret = err(e);
                    if (ret == false)
